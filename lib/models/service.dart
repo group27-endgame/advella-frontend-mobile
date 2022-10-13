485ea21da4eee71273c8268d28970e66932e7cc1
Karrtiigehyen Veerappa <71009398+Karrtii@users.noreply.github.com>
2022-10-13 23:20:10 +0200

    Implemented service model

---
+++ lib/models/service.dart
@@ -0,0 +1,18 @@
+class Service {
+
+  String title;
+  String location;
+  String price;
+  String duration;
+  String dueDate;
+  String category;
+  String timeSincePosted;
+  String description;
+  String image;
+  int numberOfBids;
+
+  Service({required this.title, required this.location, required this.price,
+    required this.duration, required this.dueDate, required this.category,
+    required this.timeSincePosted,required this.description,
+    required this.image, required this.numberOfBids});
+}
