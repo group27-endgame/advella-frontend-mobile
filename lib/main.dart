610dc4586f4f6fc0cd56230ee2a29da3a2239fd5
Karrtiigehyen Veerappa <71009398+Karrtii@users.noreply.github.com>
2022-11-06 16:11:26 +0100

    products screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,159 @@
+import 'package:advella/screens/bottom_nav_bar.dart';
+import 'package:advella/screens/browse_screen_map.dart';
+import 'package:advella/screens/filter_screen.dart';
+import 'package:advella/screens/post_task_detail_screen.dart';
+import 'package:advella/screens/subcategory_screens/cleaning_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/delivery_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/driver_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/furniture_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/gardening_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/grocery_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/laundry_subcategory_screen.dart';
+import 'package:advella/screens/subcategory_screens/pet_sitter_subcategory_screen.dart';
+import 'package:advella/screens/task_detail_screen.dart';
+import 'package:badges/badges.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'Auth/register_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    // theme: ThemeData(
+    //     textTheme: GoogleFonts.solwayTextTheme(
+    //       Theme.of().textTheme,
+    //     ),
+    // ),
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/taskDetail': (BuildContext context) => new TaskDetailScreen(),
+      '/browseMap': (BuildContext context) => new BrowseMap(),
+      '/gardeningSubcategory': (BuildContext context) => new GardeningSubcategoryScreen(),
+      '/deliverySubcategory': (BuildContext context) => new DeliverySubcategoryScreen(),
+      '/petSitterSubcategory': (BuildContext context) => new PetSitterSubcategoryScreen(),
+      '/cleaningSubcategory': (BuildContext context) => new CleaningSubcategoryScreen(),
+      '/grocerySubcategory': (BuildContext context) => new GrocerySubcategoryScreen(),
+      '/furnitureSubcategory': (BuildContext context) => new FurnitureSubcategoryScreen(),
+      '/driverSubcategory': (BuildContext context) => new DriverSubcategoryScreen(),
+      '/laundrySubcategory': (BuildContext context) => new LaundrySubcategoryScreen(),
+      '/postTaskDetail': (BuildContext context) => new PostTaskDetailScreen(),
+      '/filter': (BuildContext context) => new FilterScreen(),
+      '/register': (BuildContext context) => new RegisterScreen(),
+    },
+    home: BottomNavBar(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//
+//   int currentIndex = 0;
+//
+//   List screens = [
+//     BrowseScreen(),
+//     ProductsScreen(),
+//     MyTasksScreen(),
+//     // PostTaskScreen(),
+//     // InboxScreen(),
+//     LoginScreen(),
+//   ];
+//
+//   void onTap(int index)
+//   {
+//     setState(() {
+//       currentIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: screens[currentIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         // color: Colors.blue,
+//         // backgroundColor: Colors.transparent,
+//         onTap: onTap,
+//         currentIndex: 0,
+//         selectedItemColor: Colors.black54,
+//         unselectedItemColor: Colors.grey.withOpacity(0.5),
+//         showUnselectedLabels: false,
+//         showSelectedLabels: false,
+//         elevation: 0,
+//         items: [
+//           BottomNavigationBarItem(
+//             title: Text('Browse'),
+//             icon: Icon(Icons.home)
+//           ),
+//           BottomNavigationBarItem(
+//             title: Text('My Items'),
+//             icon: Icon(Icons.bar_chart_sharp)
+//           ),
+//           BottomNavigationBarItem(
+//             title: Text('Chat'),
+//             icon: Icon(Icons.chat)
+//           ),
+//           BottomNavigationBarItem(
+//             title: Text('Account'),
+//             icon: Icon(Icons.person)
+//           ),
+//         ],
+//       ),
+//     );
+//
+//     // return Scaffold(
+//     //   backgroundColor: Colors.white,
+//     //   body: screens[index],
+//     //   bottomNavigationBar: CurvedNavigationBar(
+//     //       color: Colors.blue,
+//     //       backgroundColor: Colors.transparent,
+//     //       items: [
+//     //         SvgPicture.asset(
+//     //           "assets/icons/home.svg",
+//     //           color: index == 0 ? Colors.white : Colors.white60,
+//     //         ),
+//     //         Icon(
+//     //           Icons.catching_pokemon_outlined,
+//     //           size: 30,
+//     //           color: index == 1 ? Colors.white : Colors.white60,
+//     //         ),
+//     //         SvgPicture.asset(
+//     //           "assets/icons/tasks.svg",
+//     //           color: index == 1 ? Colors.white : Colors.white60,
+//     //         ),
+//     //         SvgPicture.asset(
+//     //           "assets/icons/add.svg",
+//     //           color: index == 2 ? Colors.white : Colors.white60,
+//     //         ),
+//     //         Badge(
+//     //           badgeContent: Text("21", style: TextStyle(color: Colors.white),),
+//     //           child: SvgPicture.asset(
+//     //             "assets/icons/inbox.svg",
+//     //             color: index == 3 ? Colors.white : Colors.white60,
+//     //           ),
+//     //         ),
+//     //         SvgPicture.asset(
+//     //           "assets/icons/account.svg",
+//     //           color: index == 4 ? Colors.white : Colors.white60,
+//     //         ),
+//     //       ],
+//     //       height: 60,
+//     //       animationCurve: Curves.easeInOut,
+//     //       animationDuration: Duration(milliseconds: 300),
+//     //       index: index,
+//     //       onTap: (index) => setState(() {
+//     //         this.index = index;
+//     //       }),
+//     //     ),
+//     // );
+//   }
+// }
