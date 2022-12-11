e77390f64b4331fa2bdee9d8c780a1e7bcbee8b6
Karrtiigehyen Veerappa <71009398+Karrtii@users.noreply.github.com>
2022-12-11 15:37:27 +0100

    Your profile screen UI fix

--- lib/Auth/profile_screen.dart
+++ lib/Auth/profile_screen.dart
@@ -52,127 +52,132 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     {
                       return Scaffold(
                         body: SingleChildScrollView(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 100),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Profile",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 30,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 10, bottom: 10),
-                                    child: CircleAvatar(
-                                      backgroundImage: AssetImage('assets/images/profile_pic.png'),
-                                      radius: 100,
-                                    ),
-                                  ),
-                                  Text(
-                                    '${viewmodel.userName}',
+                          child: SafeArea(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(top: 15, left: 20),
+                                  child: Text(
+                                    'Your Profile',
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 25,
                                     ),
                                   ),
-                                  Text(
-                                    '${viewmodel.userEmail}',
+                                ),
+                                SizedBox(height: 55),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                                      child: CircleAvatar(
+                                        backgroundImage: AssetImage('assets/images/profile_pic.png'),
+                                        radius: 100,
+                                      ),
+                                    ),
+                                    Text(
+                                      '${viewmodel.userName}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 25,
+                                      ),
+                                    ),
+                                    Text(
+                                      '${viewmodel.userEmail}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                                      child: Text(
+                                        '${viewmodel.userDescription}',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+
+                                        ),
+                                      ),
+                                    ),
+                                    TextButton(onPressed: () async{
+                                      await UserLocalStorageService().removeUser();
+                                      Navigator.pushReplacementNamed(context, "/login");
+                                    }, child: Text("Log out")),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                  child: Text(
+                                    'Contact us',
+                                    textAlign: TextAlign.center,
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 20,
-                                      color: Colors.grey[700],
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                                    child: Text(
-                                      '${viewmodel.userDescription}',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: SizedBox(
+                                    height: 100,
+                                    child: TextField(
+                                      expands: true,
+                                      maxLines: null,
+                                      obscureText: false,
+                                      textAlign: TextAlign.start,
+                                      textAlignVertical: TextAlignVertical.top,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: "Input any problems/comments",
+                                        alignLabelWithHint: true,
                                       ),
-                                    ),
-                                  ),
-                                  TextButton(onPressed: () async{
-                                    await UserLocalStorageService().removeUser();
-                                    Navigator.pushReplacementNamed(context, "/login");
-                                  }, child: Text("Log out")),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                                child: Text(
-                                  'Contact us',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 20),
-                                child: SizedBox(
-                                  height: 100,
-                                  child: TextField(
-                                    expands: true,
-                                    maxLines: null,
-                                    obscureText: false,
-                                    textAlign: TextAlign.start,
-                                    textAlignVertical: TextAlignVertical.top,
-                                    decoration: InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      labelText: "Input any problems/comments",
-                                      alignLabelWithHint: true,
-                                    ),
-                                    onChanged: (content) {
-                                      setState(() {
-                                        _content = content;
-                                      });
-                                    },
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 10, right: 20),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    ElevatedButton(
-                                      onPressed: () async {
-                                        await viewmodel.postContactUs(_content!);
-
-                                        await Flushbar(
-                                          flushbarPosition: FlushbarPosition.TOP,
-                                          title: 'Thank you!',
-                                          message: 'Your problem/comment has been sent',
-                                          duration: Duration(seconds: 2),
-                                        ).show(context);
+                                      onChanged: (content) {
+                                        setState(() {
+                                          _content = content;
+                                        });
                                       },
-                                      child: Text(
-                                        'Send',
-                                        style: TextStyle(
-                                          fontSize: 17,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10, right: 20),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () async {
+                                          await viewmodel.postContactUs(_content!);
+
+                                          await Flushbar(
+                                            flushbarPosition: FlushbarPosition.TOP,
+                                            title: 'Thank you!',
+                                            message: 'Your problem/comment has been sent',
+                                            duration: Duration(seconds: 2),
+                                          ).show(context);
+                                        },
+                                        child: Text(
+                                          'Send',
+                                          style: TextStyle(
+                                            fontSize: 17,
+                                          ),
                                         ),
-                                      ),
-                                      style: ButtonStyle(
-                                        shape:
-                                        MaterialStateProperty.all<RoundedRectangleBorder>(
-                                          RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(10),
+                                        style: ButtonStyle(
+                                          shape:
+                                          MaterialStateProperty.all<RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(10),
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       );
